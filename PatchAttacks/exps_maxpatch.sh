f37589cb01c63b673c6522a94efed41626279d09
Ameya Joshi <ameya.joshi@nyu.edu>
2021-10-01 18:55:13 -0400

    ease of use changes to scripts

---
+++ PatchAttacks/exps_maxpatch.sh
@@ -0,0 +1,24 @@
+PATCHSIZE=224
+PATCHSIZE_384=384
+NUM_PATCHES=1
+EPSILON=1.0
+
+sbatch eval_attacks.sh vit224 /scratch/aaj458/data/ImageNet/val 8 $NUM_PATCHES 300 0.0005 $PATCHSIZE 0 $EPSILON
+sleep 2
+#sbatch eval_attacks.sh bit_152_4 /scratch/aaj458/data/ImageNet/val 100 20 1000 0.01 8
+#sleep 10
+sbatch eval_attacks.sh wide-resnet /scratch/aaj458/data/ImageNet/val 8 $NUM_PATCHES 300 0.0005 $PATCHSIZE 0 $EPSILON
+sleep 2
+sbatch eval_attacks.sh deit224 /scratch/aaj458/data/ImageNet/val 8 $NUM_PATCHES 300 0.0005 $PATCHSIZE 0 $EPSILON
+sleep 2
+sbatch eval_attacks.sh deit224_distill  /scratch/aaj458/data/ImageNet/val 8 $NUM_PATCHES 300 0.0005 $PATCHSIZE 0 $EPSILON
+sleep 2
+# #sbatch eval_attacks.sh effnet  /scratch/aaj458/data/ImageNet/val 100 20 1000 0.01 8 0 1.0
+# #sleep 10
+sbatch eval_attacks.sh vit384  /scratch/aaj458/data/ImageNet/val 8 $NUM_PATCHES 300  0.0005 $PATCHSIZE_384 0 $EPSILON
+# #sbatch eval_attacks.sh mlpb16  /scratch/aaj458/data/ImageNet/val 100 40 300  0.01 8 0 1.0
+sleep 2
+sbatch eval_attacks.sh mlpb16  /scratch/aaj458/data/ImageNet/val 8 $NUM_PATCHES 300  0.0005 $PATCHSIZE 0 $EPSILON
+sleep 2
+#sbatch eval_attacks.sh mlpb16  /scratch/aaj458/data/ImageNet/val 100 20 300  0.01 32 0 1.0
+#sleep 2
