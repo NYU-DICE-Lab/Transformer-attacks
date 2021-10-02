6f8fe5696aecffe219b88923aa15b8166f8adecb
Ameya Joshi <ameya.joshi@nyu.edu>
2021-10-02 19:05:42 -0400

    Modified the scripts for eae of use

--- PatchAttacks/exps_maxpatch.sh
+++ PatchAttacks/exps_maxpatch.sh
@@ -1,6 +1,6 @@
-PATCHSIZE=224
-PATCHSIZE_384=384
-NUM_PATCHES=1
+PATCHSIZE=32
+NUM_PATCHES_384=59 #59
+NUM_PATCHES=20 #20
 EPSILON=1.0
 
 sbatch eval_attacks.sh vit224 /scratch/aaj458/data/ImageNet/val 8 $NUM_PATCHES 300 0.0005 $PATCHSIZE 0 $EPSILON
@@ -15,7 +15,7 @@ sbatch eval_attacks.sh deit224_distill  /scratch/aaj458/data/ImageNet/val 8 $NUM
 sleep 2
 # #sbatch eval_attacks.sh effnet  /scratch/aaj458/data/ImageNet/val 100 20 1000 0.01 8 0 1.0
 # #sleep 10
-sbatch eval_attacks.sh vit384  /scratch/aaj458/data/ImageNet/val 8 $NUM_PATCHES 300  0.0005 $PATCHSIZE_384 0 $EPSILON
+sbatch eval_attacks.sh vit384  /scratch/aaj458/data/ImageNet/val 8 $NUM_PATCHES_384 300  0.0005 $PATCHSIZE 0 $EPSILON
 # #sbatch eval_attacks.sh mlpb16  /scratch/aaj458/data/ImageNet/val 100 40 300  0.01 8 0 1.0
 sleep 2
 sbatch eval_attacks.sh mlpb16  /scratch/aaj458/data/ImageNet/val 8 $NUM_PATCHES 300  0.0005 $PATCHSIZE 0 $EPSILON
