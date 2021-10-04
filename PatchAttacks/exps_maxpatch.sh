9137249dc653a72e4a0060687c7a7aaa527dcfcc
Ameya Joshi <ameya.joshi@nyu.edu>
2021-10-04 15:41:56 -0400

    Added resnet101d

--- PatchAttacks/exps_maxpatch.sh
+++ PatchAttacks/exps_maxpatch.sh
@@ -1,26 +1,22 @@
 PATCHSIZE=32
+NUM_PATCHES=20 #20
 NUM_PATCHES_384=59 #59
-NUM_PATCHES=80 #20
 EPSILON=1.0
 DPATH=/scratch/aaj458/data/ImageNet/val
+LR=0.1
 
-#sbatch eval_attacks.sh vit224 /scratch/aaj458/data/ImageNet/val 8 $NUM_PATCHES 300 0.0005 $PATCHSIZE 0 $EPSILON
-#sleep 2
-#sbatch eval_attacks.sh bit_152_4 /scratch/aaj458/data/ImageNet/val 100 20 1000 0.01 8
-#sleep 10
-#sbatch eval_attacks.sh wide-resnet /scratch/aaj458/data/ImageNet/val 8 $NUM_PATCHES 300 0.0005 $PATCHSIZE 0 $EPSILON
-#sleep 2
-#sbatch eval_attacks.sh deit224 /scratch/aaj458/data/ImageNet/val 8 $NUM_PATCHES 300 0.0005 $PATCHSIZE 0 $EPSILON
-#sleep 2
-#sbatch eval_attacks.sh deit224_distill  /scratch/aaj458/data/ImageNet/val 8 $NUM_PATCHES 300 0.0005 $PATCHSIZE 0 $EPSILON
-#sleep 2
-# #sbatch eval_attacks.sh effnet  /scratch/aaj458/data/ImageNet/val 100 20 1000 0.01 8 0 1.0
-# #sleep 10
-#sbatch eval_attacks.sh vit384  /scratch/aaj458/data/ImageNet/val 8 $NUM_PATCHES_384 300  0.0005 $PATCHSIZE 0 $EPSILON
-# #sbatch eval_attacks.sh mlpb16  /scratch/aaj458/data/ImageNet/val 100 40 300  0.01 8 0 1.0
-#sleep 2
-#sbatch eval_attacks.sh mlpb16  /scratch/aaj458/data/ImageNet/val 8 $NUM_PATCHES 300  0.0005 $PATCHSIZE 0 $EPSILON
-#sleep 2
-#sbatch eval_attacks.sh mlpb16  /scratch/aaj458/data/ImageNet/val 100 20 300  0.01 32 0 1.0
-#sleep 2
-sbatch eval_attacks.sh resnet50 /scratch/aaj458/data/ImageNet/val 8 $NUM_PATCHES 300 0.0005 $PATCHSIZE 0 $EPSILON
+sbatch eval_attacks.sh vit224 /scratch/aaj458/data/ImageNet/val 100 $NUM_PATCHES 300 0.1 $PATCHSIZE 0 $EPSILON
+sleep 2
+sbatch eval_attacks.sh wide-resnet /scratch/aaj458/data/ImageNet/val 100 $NUM_PATCHES 300 0.1 $PATCHSIZE 0 $EPSILON
+sleep 2
+sbatch eval_attacks.sh deit224 /scratch/aaj458/data/ImageNet/val 100 $NUM_PATCHES 300 0.1 $PATCHSIZE 0 $EPSILON
+sleep 2
+sbatch eval_attacks.sh deit224_distill  /scratch/asaj458/data/ImageNet/val 100 $NUM_PATCHES 300 0.1 $PATCHSIZE 0 $EPSILON
+sleep 2
+sbatch eval_attacks.sh vit384  /scratch/aaj458/data/ImageNet/val 100 $NUM_PATCHES_384 300  0.1 $PATCHSIZE 0 $EPSILON
+sleep 2
+sbatch eval_attacks.sh mlpb100  /scratch/aaj458/data/ImageNet/val 100 $NUM_PATCHES 300  0.1 $PATCHSIZE 0 $EPSILON
+sleep 2
+sbatch eval_attacks.sh resnet50 /scratch/aaj458/data/ImageNet/val 100 $NUM_PATCHES 300 0.2 $PATCHSIZE 0 $EPSILON
+sleep 2
+sbatch eval_attacks.sh resnet101d /scratch/aaj458/data/ImageNet/val 100 $NUM_PATCHES 300 0.1 $PATCHSIZE 0 $EPSILON
