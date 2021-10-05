163c18412f0dc1758b36e48f1688a135c9391ca0
Ameya Joshi <ameya.joshi@nyu.edu>
2021-10-05 11:28:43 -0400

    Latest script changes

--- PatchAttacks/eval_attacks.sh
+++ PatchAttacks/eval_attacks.sh
@@ -10,4 +10,4 @@ module load python/intel/3.8.6
 module load cuda/10.2.89
 
 source /scratch/aaj458/venv/bin/activate;
-python patch_attack_grad.py -o /scratch/aaj458/transformer_results_final8/output_mt_$1_it_$3_mp_$4_ni_$5_lr_$6_ps_$7_$9 -mt $1 --gpu 0 -dpath $2  -it $3 -mp $4 -ni $5 -clip -lr $6 -ps $7 -si $8 -eps $9
+python patch_attack_grad.py -o /scratch/aaj458/transformer_results_final_deitdistill_ps_16/output_mt_$1_it_$3_mp_$4_ni_$5_lr_$6_ps_$7_$9 -mt $1 --gpu 0 -dpath $2  -it $3 -mp $4 -ni $5 -clip -lr $6 -ps $7 -si $8 -eps $9
--- PatchAttacks/exps_maxpatch.sh
+++ PatchAttacks/exps_maxpatch.sh
@@ -1,22 +1,22 @@
-PATCHSIZE=32
-NUM_PATCHES=20 #20
-NUM_PATCHES_384=59 #59
+PATCHSIZE=1
+NUM_PATCHES=5 #20
+NUM_PATCHES_384=15 #59
 EPSILON=1.0
 DPATH=/scratch/aaj458/data/ImageNet/val
 LR=0.1
 
-sbatch eval_attacks.sh vit224 /scratch/aaj458/data/ImageNet/val 100 $NUM_PATCHES 300 0.1 $PATCHSIZE 0 $EPSILON
-sleep 2
-sbatch eval_attacks.sh wide-resnet /scratch/aaj458/data/ImageNet/val 100 $NUM_PATCHES 300 0.1 $PATCHSIZE 0 $EPSILON
-sleep 2
-sbatch eval_attacks.sh deit224 /scratch/aaj458/data/ImageNet/val 100 $NUM_PATCHES 300 0.1 $PATCHSIZE 0 $EPSILON
-sleep 2
-sbatch eval_attacks.sh deit224_distill  /scratch/asaj458/data/ImageNet/val 100 $NUM_PATCHES 300 0.1 $PATCHSIZE 0 $EPSILON
-sleep 2
-sbatch eval_attacks.sh vit384  /scratch/aaj458/data/ImageNet/val 100 $NUM_PATCHES_384 300  0.1 $PATCHSIZE 0 $EPSILON
-sleep 2
-sbatch eval_attacks.sh mlpb100  /scratch/aaj458/data/ImageNet/val 100 $NUM_PATCHES 300  0.1 $PATCHSIZE 0 $EPSILON
-sleep 2
-sbatch eval_attacks.sh resnet50 /scratch/aaj458/data/ImageNet/val 100 $NUM_PATCHES 300 0.2 $PATCHSIZE 0 $EPSILON
-sleep 2
-sbatch eval_attacks.sh resnet101d /scratch/aaj458/data/ImageNet/val 100 $NUM_PATCHES 300 0.1 $PATCHSIZE 0 $EPSILON
+# sbatch eval_attacks.sh vit224 /scratch/aaj458/data/ImageNet/val 100 $NUM_PATCHES 300 0.1 $PATCHSIZE 0 $EPSILON
+# sleep 2
+# sbatch eval_attacks.sh wide-resnet /scratch/aaj458/data/ImageNet/val 100 $NUM_PATCHES 300 0.1 $PATCHSIZE 0 $EPSILON
+# sleep 2
+# sbatch eval_attacks.sh deit224 /scratch/aaj458/data/ImageNet/val 100 $NUM_PATCHES 300 0.1 $PATCHSIZE 0 $EPSILON
+# sleep 2
+# sbatch eval_attacks.sh deit224_distill  /scratch/aaj458/data/ImageNet/val 100 $NUM_PATCHES 300 0.1 $PATCHSIZE 0 $EPSILON
+# sleep 2
+# sbatch eval_attacks.sh vit384  /scratch/aaj458/data/ImageNet/val 100 $NUM_PATCHES_384 300  0.1 $PATCHSIZE 0 $EPSILON
+# sleep 2
+# sbatch eval_attacks.sh mlpb16  /scratch/aaj458/data/ImageNet/val 100 $NUM_PATCHES 300  0.1 $PATCHSIZE 0 $EPSILON
+# sleep 2
+# sbatch eval_attacks.sh resnet50 /scratch/aaj458/data/ImageNet/val 100 $NUM_PATCHES 300 0.2 $PATCHSIZE 0 $EPSILON
+# sleep 2
+# sbatch eval_attacks.sh resnet101d /scratch/aaj458/data/ImageNet/val 100 $NUM_PATCHES 300 0.1 $PATCHSIZE 0 $EPSILON
